e49efde97437862cc783ac84417befb87f5f198f
Saubhagya <saubhagyagupta8@gmail.com>
2023-04-12 15:16:04 +0530

    created private meame" in RWOnly class created gett and sfor name and accesed it from the main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+  public static void main(String[] args){
+      //java: name has private access in com.driver.RWOnly
+      RWOnly obj = new RWOnly();
+      obj.setName("Saubhagya");
+      System.out.println(obj.getName());
+  }
 }
